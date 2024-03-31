bc0778ba78cf6b1cae9ae6be69852787bab8ba75
AdityaKane2001 <adityakane1@gmail.com>
2024-03-31 16:22:41 -0400

    added multive partially

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -1,5 +1,7 @@
 import os
+from torch.nn import ModuleList
 from .clip_encoder import CLIPVisionTower
+from .dino_encoder import DINOVisionTower
 
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
@@ -9,3 +11,20 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
 
     raise ValueError(f'Unknown vision tower: {vision_tower}')
+
+
+def build_multiple_vision_towers(vision_tower_cfg, **kwargs):
+    # Returns a list of vision towers
+    towers_pathlist = getattr(vision_tower_cfg, 'mm_multiple_vision_towers', getattr(vision_tower_cfg, 'multiple_vision_towers', None))
+    
+    vision_towers = list()
+    for towerpath in towers_pathlist:
+        is_absolute_path_exists = os.path.exists(towerpath)
+        if (is_absolute_path_exists or towerpath.startswith("openai") or towerpath.startswith("laion") or "ShareGPT4V" in towerpath) and ("clip" in towerpath):
+            vision_towers.append(CLIPVisionTower(towerpath, args=vision_tower_cfg, **kwargs))
+        elif is_absolute_path_exists or ("dino" in towerpath):
+            vision_towers.append(DINOVisionTower(towerpath, args=vision_tower_cfg, **kwargs))
+        else:
+            raise ValueError(f'Unknown vision tower: {towerpath}')
+    return ModuleList(modules=vision_towers)
+    
---
+++ scripts/v1_5/multi_ve_finetune.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "\n================================================= $(date -u) =========================================================\n"
+export WANDB_PROJECT="multi-ve-llava"
+deepspeed llava/train/multi_ve_train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path /data/data0/akane/LLaVA/data/llava_v1_5_mix665k.json \
+    --image_folder /data/data0/akane/LLaVA/data \
+    --multiple_vision_towers openai/clip-vit-large-patch14-336 facebook/dinov2-large \
+    --pretrain_mm_mlp_adapter /data/data0/akane/pretrained/mm_projector_7b.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir /data/data0/akane/multi-ve-llava-pretrained-v1.5-7b/checkpoints \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 2 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 2 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True
+
+    # --report_to wandb \
+    #     --run_name static-residual-grllava-pretrained-7b-it 
+
