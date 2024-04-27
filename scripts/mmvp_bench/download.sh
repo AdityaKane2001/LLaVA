4db04b095cf0b3c9e793eb5c9f89d63db3236fa0
AdityaKane2001 <adityakane1@gmail.com>
2024-04-27 12:20:19 -0400

    removed mmvp

--- scripts/mmvp_bench/download.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-for i in $(seq 1 301)
-do 
-	echo "Downloading $i"
-	wget -O ./MMVP\ Images/$i.jpg "https://huggingface.co/datasets/MMVP/MMVP/resolve/main/MMVP%20Images/$i.jpg?download=true"
-done
